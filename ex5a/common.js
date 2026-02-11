171bfe4c71580f173575812b897a8b37b4a4fac0
Ratchatron043 <meme311248@gmail.com>
2026-02-11 15:01:37 +0700

    refactor#2 -add paceCal fn.

--- ex5a/common.js
+++ ex5a/common.js
@@ -8,25 +8,23 @@ function validatePace(pace){
 		return {"status":0,"message":msg,"hour":0,"min":0};
 	}
 }
+function paceCal(pace,dist){
+	var min = dist * pace;
+        var hour = 0;
+        if(min > 60){
+            hour = Math.floor(min / 60);          
+            min = min % 60;
+        }
+		return{"main":main,"hour":hour};
+}
 		var msg = "";
 		var pace = parseFloat(p);
 		var dist = parseInt(d);
 	//---------------------------
-		if(isNaN(pace)){
-			msg = "Please verify that the pace you have entered is correct!!!";
-			return {"status":0,"message":msg,"hour":0,"min":0};
-		}
-		if(!(pace >= 3 && pace <= 10)){
-			msg = "Running pace must be between 3 and 10 min/km";
-			return {"status":0,"message":msg,"hour":0,"min":0};
-		}
+		var chk = validatePace(pace);
+		if(chk != null)return chk;
 	//---------------------------
-		var min = dist * pace;
-		var hour = 0;
-		if(min > 60){
-			hour = Math.floor(min / 60);          
-			min = min % 60;
-		}
+		var pc = paceCal(pace,dist);
 	//---------------------------
 		msg = "successfully calculated!"
 		return {"status":1,"message":msg,"hour":hour,"min":min};
